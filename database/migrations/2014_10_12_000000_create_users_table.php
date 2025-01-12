dc7ab73501dc472d442ff9b7693831eb077cad50
Roxana <ms.roxanarahimi@gmail.com>
2025-01-12 03:54:10 +0330

    dd

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('mobile');
+            $table->string('mobile')->unique();
             $table->timestamps();
         });
     }
